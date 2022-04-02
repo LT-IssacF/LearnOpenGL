cd312b89c1dc548d3fc6afb0cef9e887ba114d55
Jianyang Zhang <82556150+LT-IssacF@users.noreply.github.com>
2022-04-03 00:01:01 +0800

    Update main.cpp

--- src/01_hello_window/main.cpp
+++ src/01_hello_window/main.cpp
@@ -2,11 +2,16 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-// 窗口回调函数
+const GLuint width = 800;
+const GLuint height = 600;
+
+// glfw: whenever the window size changed, this callback function execute
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+    // make sure the viewport matches the new window dimensions
     glViewport(0, 0, width, height);
 }
 
+// process all input: whether relevant keys are pressed/released this frame and react accordingly
 void processInput(GLFWwindow *window) {
     if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
         glfwSetWindowShouldClose(window, true);
@@ -14,36 +19,43 @@ void processInput(GLFWwindow *window) {
 }
 
 int main() {
+    // glfw: initialize and configure
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    // glfw: window creation
+    GLFWwindow *window = glfwCreateWindow(width, height, "LearnOpenGL", NULL, NULL);
     if(window == NULL) {
         std::cout << "Failed to create window" << std::endl;
         glfwTerminate();
         return 0;
     }
     glfwMakeContextCurrent(window);
+
+    // glad: load all OpenGL function pointers
     if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return 0;
     }
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-    // 每当窗口调整大小的时候调用窗口回调函数
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    // 渲染循环
+
+    // render loop
     while(!glfwWindowShouldClose(window)) {
-        // 输入
+        // input
         processInput(window);
-        // 渲染指令
+
+        // render
         glClear(GL_COLOR_BUFFER_BIT);
-        // 检查触发事件、更新窗口动态并调用相应的回调函数
-        glfwPollEvents();
-        // 交换缓冲
+
+        // glfw: swap buffers and poll IO events(include mouse moved etc.)
         glfwSwapBuffers(window);
+        glfwPollEvents();
     }
+    
+    // glfw: clearing all previously allocated GLFW resources
     glfwTerminate();
     return 0;
 }
