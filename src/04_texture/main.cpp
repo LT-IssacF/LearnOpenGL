b9b7a68d436cfdc97b1e3172b61a8acb3f5ac352
Speirs <82556150+LT-IssacF@users.noreply.github.com>
2023-03-03 19:48:21 +0800

    Create main.cpp

---
+++ src/04_texture/main.cpp
@@ -0,0 +1,172 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <shader_s.h>
+#include <stb_image.h>
+#include <iostream>
+#include <cmath>
+
+const GLuint window_width = 800;
+const GLuint window_height = 600;
+
+GLfloat mixValue = 0.2f;
+GLboolean discoStyle = false;
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window) {
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    } if(glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
+        if(mixValue < 1.0f) {
+            mixValue += 0.0001f;
+        }
+    } if(glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
+        if(mixValue > 0.0f) {
+            mixValue -= 0.0001f;
+        }
+    } if(glfwGetKey(window, GLFW_KEY_TAB) == GLFW_PRESS) {
+        discoStyle = !discoStyle;
+    }
+}
+
+int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(window_width, window_height, "Textures", NULL, NULL);
+    if(window == NULL) {
+        std::cout << "Failed to create window" << std::endl;
+        glfwTerminate();
+        return 0;
+    }
+    glfwMakeContextCurrent(window);
+    if(!gladLoadGLLoader((GLADloadproc)(glfwGetProcAddress))) {
+        std::cout << "Failed to load GLAD" << std::endl;
+        return 0;
+    }
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    Shader myShader("../src/04_textures/shader/vertexShaderSource.vs.glsl", "../src/04_textures/shader/fragmentShaderSource.fs.glsl");
+
+    GLfloat vertices[] = {
+        //   positions           colors        texture coords
+        0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+        0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f // top left
+    };
+    GLuint indices[] = {
+        0, 1, 3, // first triangle
+        1, 2, 3 // second triangle
+    };
+    GLuint VAO, VBO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void*)0); // first parameter, layout (location = 0), aPos
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat))); // layout (location = 1), aColor
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void*)(6 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(2);
+
+    // gen texture obj
+    GLuint myTexture, myTexture2;
+    glGenTextures(1, &myTexture);
+    // bind texture
+    glBindTexture(GL_TEXTURE_2D, myTexture);
+    // set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+    // assign edge color
+    GLfloat borderColor[] = {1.0f, 1.0f, 1.0f, 1.0f};
+    glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+    // set filtering mode: use Nearest Neighbor Filtering when Minify, and Linear Filtering when Magnify
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // load texture
+    int width, height, nrChannels;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char *data = stbi_load("../resource/texture/container.jpg", &width, &height, &nrChannels, 0);
+    if(data) {
+        // Mipmap(多级渐远纹理): 在一系列的纹理图像中，后一个纹理图像的分辨率是前一个的二分之一
+        // 但是在不同级别的多级渐远纹理之间会产生不真实的生硬边界，所以在两个不同的Mipmap之间也可以使用different filtering mode
+        // 一个常见的错误是，将放大过滤的选项设置为Minmap的过滤选项之一，这样不会有任何效果
+        // generate texture
+        // .jpg是三通道
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cout << "Failed to load myTexture" << std::endl;
+    } stbi_image_free(data);
+
+    // texture2
+    glGenTextures(1, &myTexture2);
+    glBindTexture(GL_TEXTURE_2D, myTexture2);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    data = stbi_load("../resource/texture/awesomeface.png", &width, &height, &nrChannels, 0);
+    if(data) {
+        // .png是四通道
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cout << "Failed to load myTexture2" << std::endl;
+    } stbi_image_free(data);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    myShader.use();
+    // two ways to set uniforms
+    glUniform1i(myShader.getUniformLocation("myTexture"), 0);
+    myShader.setInt("myTexture2", 1);
+    
+
+    while(!glfwWindowShouldClose(window)) {
+        // input
+        processInput(window);
+
+        // render
+        glClear(GL_COLOR_BUFFER_BIT);
+        // draw
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, myTexture);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, myTexture2);
+
+        myShader.setFloat("mixValue", mixValue);
+        myShader.setBool("discoStyle", discoStyle);
+
+        myShader.use();
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    myShader.deleteProgram();
+
+    glfwTerminate();
+    return 0;
+}
